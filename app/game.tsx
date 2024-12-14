f1dea00310346710b9ffdf76f7f5362073cdecff
4sight <hgd4th@gmail.com>
2024-12-14 17:00:45 -0500

    added stylesheet

--- app/game.tsx
+++ app/game.tsx
@@ -1,14 +1,32 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
+import styles from './styles.css';
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
+      <table className={styles.board}>
+        <tr>
+          <td>d</td>
+          <td>d</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
     </View>
   );
 }
